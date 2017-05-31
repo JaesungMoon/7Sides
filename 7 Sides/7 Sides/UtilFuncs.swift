b5bbeda049968e37deea8722bd113871439c6326
Jaesung Moon <jsdekiru@gmail.com>
2017-05-31 13:09:30 +0900

    360度バー表示

---
+++ 7 Sides/7 Sides/UtilFuncs.swift
@@ -0,0 +1,14 @@
+//
+//  UtilFuncs.swift
+//  7 Sides
+//
+//  Created by 文 載盛(B04680) on 2017/05/31.
+//  Copyright © 2017年 jaesung. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+func convertDegreeToRadians(degress: CGFloat) -> CGFloat{
+    return degress * 0.0174533
+}
