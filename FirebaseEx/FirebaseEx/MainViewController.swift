c0ac3f9f9467a19b706c64cfdf5856a9b73e19ca
JaesungMoon <jsdekiru@gmail.com>
2017-08-21 00:17:32 +0900

    firebaseui pod install ※fix version 4.0 issue

--- FirebaseEx/FirebaseEx/MainViewController.swift
+++ FirebaseEx/FirebaseEx/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  FirebaseEx
 //
 //  Created by JaesungMoon on 2017/08/19.
@@ -8,17 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
