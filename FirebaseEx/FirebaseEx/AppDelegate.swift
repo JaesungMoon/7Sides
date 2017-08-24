e163280678b749c0bac3ec86de1cdb6abdd9e2b7
JaesungMoon <jsdekiru@gmail.com>
2017-08-24 23:04:31 +0900

    firebaseui login 4 set

--- FirebaseEx/FirebaseEx/AppDelegate.swift
+++ FirebaseEx/FirebaseEx/AppDelegate.swift
@@ -7,6 +7,13 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
+import FirebaseAuthUI
+import Fabric
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,14 +22,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = MainViewController()
         
         
         
         return true
     }
-
+    
+    @available(iOS 9.0, *)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?
+        return self.handleOpenUrl(url, sourceApplication: sourceApplication)
+    }
+    
+    @available(iOS 8.0, *)
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return self.handleOpenUrl(url, sourceApplication: sourceApplication)
+    }
+    func handleOpenUrl(_ url: URL, sourceApplication: String?) -> Bool {
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
+    }
 
 }
 
--- FirebaseEx/FirebaseEx/MainViewController.swift
+++ FirebaseEx/FirebaseEx/MainViewController.swift
@@ -7,15 +7,52 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+
+import FirebaseGoogleAuthUI
+
+import FirebaseFacebookAuthUI
+import FirebaseTwitterAuthUI
 
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .green
+        
+        checkIfUserIsLoggedIn()
     }
 
-
-
+    private func checkIfUserIsLoggedIn() {
+        if Auth.auth().currentUser?.uid == nil {
+            perform(#selector(handleShowLogin), with: nil, afterDelay: 0)
+        } else {
+            print("ログイン中 id = \(Auth.auth().currentUser!.uid)")
+            print("ログイン中 name = \(String(describing: Auth.auth().currentUser?.displayName))")
+            
+            
+            //もし既存ユーザーからログアウトしたい場合は下記を実行してください。
+            //            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        }
+    }
+    func handleShowLogin() {
+        let authUI: FUIAuth? = FUIAuth.defaultAuthUI()
+        authUI?.providers = getListOfIDPs()
+        
+        let controller = authUI!.authViewController()
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    private func getListOfIDPs() -> [FUIAuthProvider] {
+        var providers = [FUIAuthProvider]()
+        providers.append(FUIGoogleAuth())
+        providers.append(FUITwitterAuth())
+        providers.append(FUIFacebookAuth())
+        
+        return providers
+    }
 }
 
