484554002a63b67c5e45eb8c65563f91f4defa42
JaesungMoon <jsdekiru@gmail.com>
2018-05-08 02:29:41 +0900

    custompath custom WIP

---
+++ FBStreamAnimation/FBStreamAnimation/Util.swift
@@ -0,0 +1,29 @@
+//
+//  Util.swift
+//
+//  Created by JaesungMoon on 2017/08/15.
+
+import UIKit
+
+public func LOG_TRACE<T>(_ message: T, _ file: String = #file, _ function: String = #function, _ line: Int = #line) {
+    #if DEBUG
+        var logMessage = ""
+        if let stringObject = message as? String {
+            logMessage = stringObject
+        } else {
+            let stringObject = String(describing: message)
+            logMessage = stringObject
+        }
+        
+        let fileExtension = file.ns.lastPathComponent.ns.pathExtension
+        let fileName = file.ns.lastPathComponent.ns.deletingPathExtension
+        
+        print(fileName + "." + fileExtension + ":\(line) " + function + ": " + logMessage)
+    #else
+        
+    #endif
+}
+
+private extension String {
+    var ns: NSString { return self as NSString }
+}
--- FBStreamAnimation/FBStreamAnimation/ViewController.swift
+++ FBStreamAnimation/FBStreamAnimation/ViewController.swift
@@ -13,24 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let curvedView = CurvedView(frame: view.frame)
-//        curvedView.backgroundColor = .yellow
-//        view.addSubview(curvedView)
+        let curvedView = CurvedView(frame: CGRect(x: -view.frame.width, y: 0, width: view.frame.width*2, height: view.frame.height))
+        LOG_TRACE(view.frame)
+        curvedView.backgroundColor = .yellow
+        view.addSubview(curvedView)
+
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: [.repeat, .curveLinear,], animations: {
+            LOG_TRACE("anim curvedView.center.x = \(curvedView.center.x)")
+            curvedView.center.x += self.view.frame.width
+            curvedView.path = customPath(x: curvedView.frame.width)
+        }, completion: { result in
+            LOG_TRACE("cimp curvedView.center.x = \(curvedView.center.x)")
+        })
         
+//        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
         
-        
-        
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     
     func handleTap() {
-        (0...10).forEach{ (_) in
+        (0...1).forEach{ (_) in
             generateAnimatedViews()
-            
         }
     }
     
     fileprivate func generateAnimatedViews() {
+        LOG_TRACE("")
         let imageView = UIImageView(image: #imageLiteral(resourceName: "facebook"))
         let dimension = 20 + drand48() * 10
         
@@ -38,7 +45,7 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
         
         let animation = CAKeyframeAnimation(keyPath: "position")
-        animation.path = customPath().cgPath
+        animation.path = customPath(x: 375).cgPath
         animation.duration = 2
         animation.fillMode = kCAFillModeForwards
         animation.isRemovedOnCompletion = false
@@ -48,28 +55,41 @@ class ViewController: UIViewController {
 
 }
 
-func customPath() -> UIBezierPath {
+func customPath(x: CGFloat) -> UIBezierPath {
     let path = UIBezierPath()
-    
-    path.move(to: CGPoint(x: 0, y: 200))
-    
-    let endPoint = CGPoint(x: 400, y: 200)
-    
-    let cp1 = CGPoint(x: 100, y: 100)
-    let cp2 = CGPoint(x: 200, y: 300)
+    let y: CGFloat = CGFloat((arc4random() % 5) * 100)
+    LOG_TRACE("y = \(y)")
+    path.move(to: CGPoint(x: 0, y: y))
+
+    let endPoint = CGPoint(x: x/2, y: y)
+    let cp1 = CGPoint(x: x / 2 / 3, y: y - 100)
+    let cp2 = CGPoint(x: x / 2 / 3 * 2, y: y + 100)
     path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
     
+    let endPoint2 = CGPoint(x: x, y: y)
+    let cp21 = CGPoint(x: x / 2 + x / 2 / 3, y: y - 100)
+    let cp22 = CGPoint(x: x / 2 + x / 2 / 3 * 2, y: y + 100)
+    path.addCurve(to: endPoint2, controlPoint1: cp21, controlPoint2: cp22)
     
     return path
 }
 
 class CurvedView: UIView {
+    var path: UIBezierPath!
+    var width: CGFloat!
     override func draw(_ rect: CGRect) {
         //do some fancy curve drawing
-    
-        let path = customPath()
+        width = rect.width
+        
+        path = customPath(x: rect.width)
         path.lineWidth = 3
         path.stroke()
         
+    }
+    func setPath(y: CGFloat) {
+        LOG_TRACE("y = \(y)")
+        path = customPath(x: width)
+        path.lineWidth = 3
+        path.stroke()
     }
 }
