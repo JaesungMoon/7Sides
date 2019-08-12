0f40310884158ab54520733de7ca55b96afff56a
JaesungMoon <jsdekiru@gmail.com>
2019-08-12 13:28:17 +0900

    add CustomFontEx
    just add font file and use from UILabel

---
+++ CustomFontEx/CustomFontEx/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CustomFontEx
+//
+//  Created by JaesungMoon on 2019/08/12.
+//  Copyright © 2019 jaesung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+
+
+}
+
